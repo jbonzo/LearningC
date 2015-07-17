f960abd7ae850a5e81990a1e79621c2db23addda
Ricky Barillas <rbarillas3@gatech.edu>
2015-07-17 12:13:37 -0400

    Revert "Initial Commit"
    This reverts commit 14047f0c8fb409170f8c2e48e3d52ceb4cb1a406.

--- Chapter2.c
+++
@@ -1,7 +0,0 @@
-enum Weekday {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
-
-enum Weekday today = Wednesday;
-
-/*enum Weekday {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday} today, tomorrow;*/
-/*enum Weekday {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday} today = Monday, tomorrow = Tuesday;*/
-/*enum Weekday {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday} today = Monday, tomorrow = today + 1;*/
